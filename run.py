fb17dbad35d5e6771b0493a9fcb0f97fc3cff5b2
Julija Dociene <julijablazauskaite@gmail.com>
2022-05-04 14:42:17 +0300

    download and unzip data

---
+++ run.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import subprocess
+from scripts import unzip_data
+import os.path
+
+#import dvc.yaml
+#run dvc.yaml file
+
+
+
+#save output to results location
+    #first arg is the name of the file
+    #second arg notes that the file is open to write to it
+with open(r'.\results\readme.txt', 'w') as f:
+    
+
+    #unzip downloaded data
+    subprocess.call("unzip_data.py", shell=True)
+
+    #import train.csv file
+    df = pd.read_csv(r'.\data\train.csv')
+    #df.head(df)
+
+    #Feature Engineering
+    #Checking for missing values
+    print(df.isnull().count)
+
+
+#Rezultatų masyvas 
+    df_results = 1
+
+#write results to '.\results\results.txt'
+    f.write(df_results)
